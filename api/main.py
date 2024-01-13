5ba5dd1e97a6f7b0d6999a2d3d05693f912b6242
Adan Mercado <adanmercado.dev@gmail.com>
2024-01-13 00:06:11 -0600

    Update app name

--- api/main.py
+++ api/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 
-app = FastAPI(title='Book Store API',
+app = FastAPI(title='Stock tracker API',
               version='1.0.0')
 
 @app.get('/', response_class=JSONResponse)
 def info():
     return JSONResponse(status_code=200, content={
-        'app_name': 'Book Store API',
+        'app_name': 'Stock tracker API',
         'version': '1.0.0',
         'author': 'Adan Mercado',
         'email': 'adanmercado.dev@gmail.com'
