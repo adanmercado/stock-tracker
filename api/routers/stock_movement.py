d5290a45c6ba31a4b608e77383aab677dccdd27a
Adan Mercado <adanmercado.dev@gmail.com>
2024-01-15 21:26:53 -0600

    Add endpoint to register a stock movement

--- api/routers/stock_movement.py
+++ api/routers/stock_movement.py
@@ -15,3 +15,12 @@ stock_movement_router = APIRouter(tags=['Stock'])
 def get_all_stock_movements(db: Session = Depends(db_session)):
     movements = db.query(StockMovementModel).options(joinedload(StockMovementModel.product)).all()
     return JSONResponse(status_code=200, content=jsonable_encoder(movements))
+
+@stock_movement_router.post('/stock', response_model=StockMovement)
+def create_stock_movement(movement: StockMovement, db: Session = Depends(db_session)):
+    stock_movement = StockMovementModel(**movement.model_dump())
+    db.add(stock_movement)
+    db.commit()
+    db.refresh(stock_movement)
+
+    return JSONResponse(status_code=201, content=jsonable_encoder(stock_movement))
